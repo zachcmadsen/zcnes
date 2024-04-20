5e65e9a3fb46d3f8dc42c3d2cfc7b9dd54de4e2b
Zach Madsen <zach@zachmadsen.com>
2024-04-19 22:36:22 -0700

    Fix an argument name

--- src/core/nes.c
+++ src/core/nes.c
@@ -4,7 +4,7 @@
 
 static bool overflowing_add(uint8_t a, uint8_t b, uint8_t *out) {
 #if defined(__has_builtin) && __has_builtin(__builtin_add_overflow)
-    return __builtin_add_overflow(a, b, result);
+    return __builtin_add_overflow(a, b, out);
 #else
     *out = a + b;
     return *out < a;
