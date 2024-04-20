5957a618402545d6edf0194e224fc99d7958851b
Zach Madsen <zach@zachmadsen.com>
2024-04-19 22:53:26 -0700

    Add the rest of the addressing modes

--- src/core/nes.c
+++ src/core/nes.c
@@ -21,6 +21,10 @@ static uint8_t eat_byte(struct zc_nes *nes) {
     return data;
 }
 
+static void write_byte(struct zc_nes *nes, uint16_t addr, uint8_t data) {
+    nes->cpu.ram[addr] = data;
+}
+
 static void abu(struct zc_nes *nes) {
     uint8_t low = eat_byte(nes);
     uint8_t high = eat_byte(nes);
@@ -29,22 +33,38 @@ static void abu(struct zc_nes *nes) {
 
 static void abx_r(struct zc_nes *nes) {
     uint8_t low = eat_byte(nes);
-    bool page_cross = overflowing_add(low, nes->cpu.x, &low);
+    bool overflow = overflowing_add(low, nes->cpu.x, &low);
     uint8_t high = eat_byte(nes);
-    if (page_cross) {
+    if (overflow) {
         read_byte(nes, low | high << 8);
     }
-    nes->cpu.ea = low | (uint8_t)(high + page_cross) << 8;
+    nes->cpu.ea = low | (uint8_t)(high + overflow) << 8;
+}
+
+static void abx_w(struct zc_nes *nes) {
+    uint8_t low = eat_byte(nes);
+    bool overflow = overflowing_add(low, nes->cpu.x, &low);
+    uint8_t high = eat_byte(nes);
+    read_byte(nes, low | high << 8);
+    nes->cpu.ea = low | (uint8_t)(high + overflow) << 8;
 }
 
 static void aby_r(struct zc_nes *nes) {
     uint8_t low = eat_byte(nes);
-    bool page_cross = overflowing_add(low, nes->cpu.y, &low);
+    bool overflow = overflowing_add(low, nes->cpu.y, &low);
     uint8_t high = eat_byte(nes);
-    if (page_cross) {
+    if (overflow) {
         read_byte(nes, low | high << 8);
     }
-    nes->cpu.ea = low | (high + page_cross) << 8;
+    nes->cpu.ea = low | (uint8_t)(high + overflow) << 8;
+}
+
+static void aby_w(struct zc_nes *nes) {
+    uint8_t low = eat_byte(nes);
+    bool overflow = overflowing_add(low, nes->cpu.y, &low);
+    uint8_t high = eat_byte(nes);
+    read_byte(nes, low | high << 8);
+    nes->cpu.ea = low | (uint8_t)(high + overflow) << 8;
 }
 
 static void idx(struct zc_nes *nes) {
@@ -54,6 +74,26 @@ static void idx(struct zc_nes *nes) {
     uint8_t low = read_byte(nes, ptr);
     uint8_t high = read_byte(nes, (uint8_t)(ptr + 1));
     nes->cpu.ea = low | high << 8;
+}
+
+static void idy_r(struct zc_nes *nes) {
+    uint8_t ptr = eat_byte(nes);
+    uint8_t low = read_byte(nes, ptr);
+    bool overflow = overflowing_add(low, nes->cpu.y, &low);
+    uint8_t high = read_byte(nes, (uint8_t)(ptr + 1));
+    if (overflow) {
+        read_byte(nes, low | high << 8);
+    }
+    nes->cpu.ea = low | (uint8_t)(high + overflow) << 8;
+}
+
+static void idy_w(struct zc_nes *nes) {
+    uint8_t ptr = eat_byte(nes);
+    uint8_t low = read_byte(nes, ptr);
+    bool overflow = overflowing_add(low, nes->cpu.y, &low);
+    uint8_t high = read_byte(nes, (uint8_t)(ptr + 1));
+    read_byte(nes, low | high << 8);
+    nes->cpu.ea = low | (uint8_t)(high + overflow) << 8;
 }
 
 static void imm(struct zc_nes *nes) {
@@ -101,6 +141,10 @@ static void jmp(struct zc_nes *nes) {
     nes->cpu.pc = nes->cpu.ea;
 }
 
+static void sta(struct zc_nes *nes) {
+    write_byte(nes, nes->cpu.ea, nes->cpu.a);
+}
+
 void cpu_step(struct zc_nes *nes) {
     uint8_t opc = eat_byte(nes);
     if (opc == 0x6C) {
@@ -118,6 +162,9 @@ void cpu_step(struct zc_nes *nes) {
     } else if (opc == 0xAD) {
         abu(nes);
         lda(nes);
+    } else if (opc == 0xB1) {
+        idy_r(nes);
+        lda(nes);
     } else if (opc == 0xB5) {
         zpx(nes);
         lda(nes);
@@ -130,6 +177,27 @@ void cpu_step(struct zc_nes *nes) {
     } else if (opc == 0xBD) {
         abx_r(nes);
         lda(nes);
+    } else if (opc == 0x85) {
+        zpg(nes);
+        sta(nes);
+    } else if (opc == 0x8D) {
+        abu(nes);
+        sta(nes);
+    } else if (opc == 0x9D) {
+        abx_w(nes);
+        sta(nes);
+    } else if (opc == 0x95) {
+        zpx(nes);
+        sta(nes);
+    } else if (opc == 0x99) {
+        aby_w(nes);
+        sta(nes);
+    } else if (opc == 0x81) {
+        idx(nes);
+        sta(nes);
+    } else if (opc == 0x91) {
+        idy_w(nes);
+        sta(nes);
     }
 }
 
